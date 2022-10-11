f0e444d19391b9e870d4811b06263902377fcd39
Andrey Razumov <63653491+amacumara@users.noreply.github.com>
2022-10-12 00:03:31 +0300

    Небольшие правки и комментарии

--- fileNameOnPhoto.jsx
+++ fileNameOnPhoto.jsx
@@ -2,13 +2,13 @@ app.preferences.rulerUnits = Units.CM;
 
 var doc = app.activeDocument;
 
-var txt_layer = doc.artLayers.add();
+var txtLayer = doc.artLayers.add();
 
-txt_layer.kind = LayerKind.TEXT;
+txtLayer.kind = LayerKind.TEXT;
 
 var colorSet = new SolidColor; with (colorSet.rgb) { red = 255; green = 255; blue = 255; }  //Цвет надписи
 
-with (txt_layer.textItem)
+with (txtLayer.textItem)
 {
 font = "MyriadPro-Regular"; //Шрифт, важно правильное написание
 position = Array(doc.width /2, doc.height *5/6); //Положение на холсте (относительное, для моей задачи), можно делать абсолютным
@@ -22,8 +22,8 @@ fauxBold = true;
 
 var imgName = doc.name.substring (0, doc.name.length - 4); //Убираю расширение файла, можно элегантней через функцию относительно "."
 
-txt_layer.textItem.contents = imgName;
+txtLayer.textItem.contents = imgName;
 
-//txt_layer.name = "text" //Имя создаваемого слоя, по умолчанию не использовал
+//txtLayer.name = "text" //Имя создаваемого слоя, по умолчанию не использовал
 
  doc.flatten(); //Функция схлопывания слоев
