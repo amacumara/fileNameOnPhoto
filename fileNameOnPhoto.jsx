eb7dcc466fdc0e90858e8b7d6289d9bdaaa7d906
Andrey Razumov <63653491+amacumara@users.noreply.github.com>
2022-10-12 00:01:01 +0300

    Добавил описание к скрипту
    Добавил описание для тех, кто не разбирается, чтобы проще было менять параметры

--- fileNameOnPhoto.jsx
+++ fileNameOnPhoto.jsx
@@ -6,25 +6,24 @@ var txt_layer = doc.artLayers.add();
 
 txt_layer.kind = LayerKind.TEXT;
 
-var colorSet = new SolidColor; with (colorSet.rgb) { red = 255; green = 255; blue = 255; }
+var colorSet = new SolidColor; with (colorSet.rgb) { red = 255; green = 255; blue = 255; }  //Цвет надписи
 
 with (txt_layer.textItem)
 {
-font = "MyriadPro-Regular";
-position = Array(doc.width /2, doc.height *5/6);
-justification = Justification.CENTER;
-size = doc.height/20;
+font = "MyriadPro-Regular"; //Шрифт, важно правильное написание
+position = Array(doc.width /2, doc.height *5/6); //Положение на холсте (относительное, для моей задачи), можно делать абсолютным
+justification = Justification.CENTER; //Центрирование
+size = doc.height/20; //Размер надписи (относительный, для моей задачи), можно делать абсолютным
 color = colorSet;
 
 antiAliasMethod = AntiAlias.STRONG;
 fauxBold = true;
 }
 
-var imgName = doc.name.substring (0, doc.name.length - 4);
+var imgName = doc.name.substring (0, doc.name.length - 4); //Убираю расширение файла, можно элегантней через функцию относительно "."
 
 txt_layer.textItem.contents = imgName;
 
-txt_layer.name = "text"
+//txt_layer.name = "text" //Имя создаваемого слоя, по умолчанию не использовал
 
-
-// doc.flatten();
+ doc.flatten(); //Функция схлопывания слоев
